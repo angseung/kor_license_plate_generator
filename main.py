ab2a3cbe4e5a151a12bc1d8ec92c206145c5e898
angseung <angseung@outlook.com>
2023-01-05 22:39:35 +0900

    TODO: generate missing data samples

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 from plate_generator import ImageGenerator
 
-generator = ImageGenerator(save_path="./DB_new")
+generator = ImageGenerator(save_path="./DB")
 
-# # 2digit white short
-# for char in range(35):
-#     for _ in range(320):
-#         generator.white_short_2digits(char, True)
-#
-# # 2digit white long
-# for char in range(35):
-#     for _ in range(320):
-#         generator.white_long_2digits(char, True)
-#
-# # 3digit white long
-# for char in range(35):
-#     for _ in range(320):
-#         generator.white_long_3digits(char, True)
+# 2digit white short
+for char in range(35):
+    for _ in range(320):
+        generator.white_short_2digits(char, True)
+
+# 2digit white long
+for char in range(35):
+    for _ in range(320):
+        generator.white_long_2digits(char, True)
+
+# 3digit white long
+for char in range(35):
+    for _ in range(320):
+        generator.white_long_3digits(char, True)
 
 # electronic long
 for char in range(40):
     for _ in range(320):
         generator.electronic_long(char, True)
 
-# # yellow long
-# for char in range(5):
-#     for region in range(17):
-#         for _ in range(200):
-#             generator.yellow_long(region, char, True)
-#
-# # yellow short
-# for char in range(5):
-#     for region in range(17):
-#         for _ in range(200):
-#             generator.yellow_short(region, char, True)
+# yellow long
+for char in range(5):
+    for region in range(17):
+        for _ in range(200):
+            generator.yellow_long(region, char, True)
+
+# yellow short
+for char in range(5):
+    for region in range(17):
+        for _ in range(200):
+            generator.yellow_short(region, char, True)
 
 # green short
 for char in range(37):
