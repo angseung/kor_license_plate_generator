93fff6b392289e1d809460dfdc2944bad5129416
angseung <swim@surromind.ai>
2023-05-02 08:43:50 +0900

    Debugged codes in bbox rotation example

--- rotate_bbox.py
+++ rotate_bbox.py
@@ -7,13 +7,14 @@ height, width = img_ori.shape[:2]
 labels = parse_label("test.txt")
 labels_yolo = label_voc2yolo(labels, w=width, h=height)
 
-img, labels_rot = rotate_img_and_bboxes(img_ori, labels_yolo, angle=30, bg_color="black")
-labels_voc = label_yolo2voc(labels_rot, w=width, h=height)
+img_rot, labels_rot = rotate_img_and_bboxes(img_ori, labels_yolo, angle=30, bg_color="black")
+width_new, height_new = img_rot.shape[:2]  # get a new shape of the rotated image
+labels_voc = label_yolo2voc(labels_rot, w=width_new, h=height_new)
 img_ori_bbox = draw_bbox_on_img(img_ori, labels_yolo)
-img_rot_bbox = draw_bbox_on_img(img, labels_voc)
+img_rot_bbox = draw_bbox_on_img(img_rot, labels_voc)
 
-plt.subplot(211)
+plt.subplot(121)
 plt.imshow(img_ori_bbox)
-plt.subplot(212)
+plt.subplot(122)
 plt.imshow(img_rot_bbox)
 plt.show()
